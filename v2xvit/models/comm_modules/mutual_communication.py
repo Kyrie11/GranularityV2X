a96c43152573669cbfcf5ddb61a086718857a737
Kyrie11 <770790858@qq.com>
2025-05-18 23:50:28 +0800

    .

--- v2xvit/models/comm_modules/mutual_communication.py
+++ v2xvit/models/comm_modules/mutual_communication.py
@@ -253,7 +253,7 @@ class Communication(nn.Module):
                 y_idx = (agent_coords[:, 2] / self.discrete_ratio).long().clamp(0, H - 1)  # [K]
 
                 # ==== 生成三维掩码索引 ====
-                voxel_mask = sparse_points_mask[:, y_idx, x_idx].any(dim=0)
+                voxel_mask = sparse_points_mask[:, y_idx, x_idx]
                 print("voxel_mask:", len(voxel_mask))
                 if False in voxel_mask:
                     print("确实存在False")
