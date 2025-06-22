9b8d85df22ba7b923c899cc1c6245afc9f870cb9
Kyrie11 <770790858@qq.com>
2025-06-22 13:56:46 +0800

    .

--- v2xvit/models/sub_modules/mixed_feature_flow.py
+++ v2xvit/models/sub_modules/mixed_feature_flow.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torchvision.ops import RoIAlign  # For ObjectContextExtractor
 
 
 # Assuming BasicConvBlock and other utilities are defined elsewhere or standard
