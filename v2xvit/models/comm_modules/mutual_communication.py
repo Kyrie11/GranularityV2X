e1a0a51bd99dd7e4d70e95f31d256497f47da7dc
Kyrie11 <770790858@qq.com>
2025-06-28 15:57:19 +0800

    .

--- v2xvit/models/comm_modules/mutual_communication.py
+++ v2xvit/models/comm_modules/mutual_communication.py
@@ -456,7 +456,7 @@ class AdvancedCommunication(nn.Module):
             #计算通信量
             cost_reshaped = self.cost_vector.view(1, 3, 1, 1)
             volume_map = transmission_mask.float()*cost_reshaped
-            print("volume_map=", volume_map)
+            print("sum=", torch.sum(volume_map))
             total_communication_volume += torch.sum(volume_map)
 
             # if self.training:
