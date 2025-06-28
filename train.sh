04b5c03ff873f11b10b6dab36e76afbb3a478f0e
Kyrie11 <770790858@qq.com>
2025-06-29 00:43:22 +0800

    .

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
-CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=2 python v2xvit/tools/train.py --hypes_yaml /home/senzeyu2/code/GranularityV2X/v2xvit/hypes_yaml/how2comm/v2xset_how2comm_stcformer.yaml
+CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=2 python v2xvit/tools/train.py --hypes_yaml /home/senzeyu2/code/GranularityV2X/v2xvit/hypes_yaml/how2comm/v2xset_how2comm_stcformer.yaml  --model_dir v2xset_how2comm_stcformer_opv2v_2025_06_29_00_35_58
 
