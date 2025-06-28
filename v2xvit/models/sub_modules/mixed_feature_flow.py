cdff13a10c8621f520f942e3d78c670e81fe1997
Kyrie11 <770790858@qq.com>
2025-06-28 09:57:04 +0800

    .

--- v2xvit/models/sub_modules/mixed_feature_flow.py
+++ v2xvit/models/sub_modules/mixed_feature_flow.py
@@ -152,7 +152,7 @@ class ContextFusionMotionPredictor(nn.Module):
         # --- 3. 融合上下文 ---
         delay = torch.tensor(delay, dtype=torch.long)
         delay = delay.unsqueeze(0)
-        delay = torch.tensor(delay * B, dtype=torch.long)
+        delay = delay * B
         print("delay.shape=", delay.shape)
         delay_emb = self.delay_embedding(delay)
         print("delay_emb.shape=", delay_emb.shape)
