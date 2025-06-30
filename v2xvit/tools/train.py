c3563aae9e2bf860572ed4b0e27f809e4e3edffa
Kyrie11 <770790858@qq.com>
2025-06-30 23:19:22 +0800

    .

--- v2xvit/tools/train.py
+++ v2xvit/tools/train.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import torch
 torch.multiprocessing.set_sharing_strategy('file_system')
 import os,time
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:64"
 torch.autograd.set_detect_anomaly(True)
 import tqdm
 from torch.utils.data import DataLoader, DistributedSampler
