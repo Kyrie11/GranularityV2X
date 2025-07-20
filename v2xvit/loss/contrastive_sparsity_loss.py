bebed86650181acfef10f47dadde27960147a8c4
Kyrie11 <770790858@qq.com>
2025-07-21 01:20:14 +0800

    .

--- v2xvit/loss/contrastive_sparsity_loss.py
+++ v2xvit/loss/contrastive_sparsity_loss.py
@@ -72,7 +72,9 @@ class ContrastiveSparsityLoss(nn.Module):
                 all_dense_keys = [dense_g1_keys, dense_g2_keys, dense_g3_keys]
             for i in range(1,  cav_num):
                 unique_mask = decision_mask[i-1]
+                print("decision_mask:", unique_mask)
                 for decision in unique_mask:
+                    print("decision:", decision)
                     decision_val = decision.item()
                     if decision_val == 0:
                         continue
