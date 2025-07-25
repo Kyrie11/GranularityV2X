4626ab68d9515e296efe964fc3bcb379a3aeacac
Kyrie11 <770790858@qq.com>
2025-07-26 04:49:46 +0800

    .

--- v2xvit/models/point_pillar_how2comm.py
+++ v2xvit/models/point_pillar_how2comm.py
@@ -104,7 +104,7 @@ class PointPillarHow2comm(nn.Module):
         pairwise_t_matrix = current_data_dict['pairwise_t_matrix'].clone().detach()
         record_len = current_data_dict['record_len']
         short_his_g1, short_his_g2, short_his_g3 = self.get_histroy_granularity(short_term)
-        print(f"short_his_g1.shape={short_his_g1.shape}")
+        print(f"short_his_g1.shape={short_his_g1[0].shape}")
         print(f"short_his_g2.shape={short_his_g2.shape}")
         long_his_g1, long_his_g2, long_his_g3 = self.get_histroy_granularity(long_term)
         short_his = [short_his_g1, short_his_g2, short_his_g3]
@@ -149,6 +149,8 @@ class PointPillarHow2comm(nn.Module):
             batch_dict = self.pillar_vfe(batch_dict)
             batch_dict = self.scatter(batch_dict)
             batch_dict = self.backbone(batch_dict)
+            spatial_features = batch_dict['spatial_features']
+            print(f"spatial_features.shape={spatial_features.shape}")
             spatial_features_2d = batch_dict['spatial_features_2d']
             # downsample feature to reduce memory
             if self.shrink_flag:
