13c7f28033b6fc03825a42c187e1905ed9a4fdba
Kyrie11 <770790858@qq.com>
2025-07-27 15:28:12 +0800

    .

--- v2xvit/models/comm_modules/mutual_communication.py
+++ v2xvit/models/comm_modules/mutual_communication.py
@@ -172,6 +172,9 @@ class AdvancedCommunication(nn.Module):
         collab_sparse_g1 = collab_g1 * sparse_maps[:, 0:1, :, :]
         collab_sparse_g2 = collab_g2 * sparse_maps[:, 1:2, :, :]
         collab_sparse_g3 = collab_g3 * sparse_maps[:, 2:3, :, :]
+        print("collab_sparse_g1.shape=",collab_sparse_g1.shape)
+        print("collab_sparse_g2.shape=",collab_sparse_g2.shape)
+        print("collab_sparse_g3.shape=",collab_sparse_g3.shape)
 
         sparse_g1 = torch.stack([ego_g1, collab_sparse_g1], dim=0)
         sparse_g2 = torch.stack([ego_g2, collab_sparse_g2], dim=0)
