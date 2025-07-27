e19b5a2e972358b0432c18f751ff421e25d1918d
Kyrie11 <770790858@qq.com>
2025-07-27 17:27:04 +0800

    .

--- train.sh
+++ train.sh
@@ -1,3 +1,4 @@
+python v2xvit/tools/train.py --hypes_yaml v2xvit/hypes_yaml/how2comm/v2xset_how2comm_stcformer.yaml --half
 
-torchrun --nproc_per_node=2 --standalone v2xvit/tools/train.py --hypes_yaml v2xvit/hypes_yaml/how2comm/v2xset_how2comm_stcformer.yaml --half
+#torchrun --nproc_per_node=2 --standalone v2xvit/tools/train.py --hypes_yaml v2xvit/hypes_yaml/how2comm/v2xset_how2comm_stcformer.yaml --half
 
